c68848bee066c50de664c012fd33845438232093
Eyad Shalabi <>
2021-03-08 10:56:55 +0200

    add scene delegate

---
+++ IDTest/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  IDTest
+//
+//  Created by Classera on 3/8/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+
+}
+
